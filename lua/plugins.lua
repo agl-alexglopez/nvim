a8ba2579b8ab7348c4fd1bd8d7ef283b0bf0120d
Alex G. Lopez <agl.alexglopez@gmail.com>
2022-01-20 22:03:17 -0800

    indent fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,12 @@
 -- Plugins
 return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
-	use 'folke/tokyonight.nvim'
+  use 'folke/tokyonight.nvim'
 
 end)
