e756a431e6443c061550efb1b0088d302fa7095d
Alex Lopez <agl.alexglopez@gmail.com>
2022-06-19 08:30:56 -0700

    fix permissions on servers used

--- lua/setups.lua
+++ lua/setups.lua
@@ -58,13 +58,18 @@ npairs.setup({
 })
 
 -- ---------------------------completions and lsp----------
+
 local lsp_installer = require("nvim-lsp-installer")
 
 -- Register a handler that will be called for each installed server when it's ready
 -- (i.e. when installation is finished
 -- or if the server is already installed).
 lsp_installer.on_server_ready(function(server)
-    local opts = {}
+    -- local opts = {},
+
+    -- Equivalent to adding each of the servers you are using to path so they haver permissions to
+    -- run and are fed to the lspconfig.
+    server:setup {'clangd', 'pyright'}
 
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
@@ -75,7 +80,7 @@ lsp_installer.on_server_ready(function(server)
     -- necessary properties
     -- before passing it onwards to lspconfig.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
+    -- server:setup(opts)
 end)
 
 require('cmp').setup({
@@ -169,11 +174,18 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
+local lsp_flags = {
+  debounce_text_changes = 150,
+}
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['clangd'].setup {
-  capabilities = capabilities
+  capabilities = capabilities,
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
 require('lspconfig')['pyright'].setup {
-  capabilities = capabilities
+  capabilities = capabilities,
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
