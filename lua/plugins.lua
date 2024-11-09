d1a9b853d60c09ba87e798f64ff7f45b1092a590
Alexander Lopez <agl.alexglopez@gmail.com>
2024-11-08 22:36:39 -0800

    fix files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,14 @@
 -- Install lazy if not installed to prevent plugin errors on new nvim config.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,82 +21,82 @@ vim.g.maplocalleader = " "
 -- Plugins
 require("lazy").setup({
 
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
 
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here --
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
-	},
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here --
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
+    },
 
-	"BurntSushi/ripgrep",
+    "BurntSushi/ripgrep",
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
 
-	{
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	},
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
 
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-	},
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		-- or                            , tag = '0.1.CHOOSE_LATEST_TAG_FROM_REPO'
-		dependencies = { { "nvim-lua/plenary.nvim" } },
-	},
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        -- or                            , tag = '0.1.CHOOSE_LATEST_TAG_FROM_REPO'
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+    },
 
-	"L3MON4D3/LuaSnip", -- Snippets plugin
-	"saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
-	"hrsh7th/nvim-cmp", -- Autocompletion plugin
-	"hrsh7th/cmp-nvim-lsp-signature-help",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-nvim-lua", -- For writing lua neovim specific
-	"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-	"hrsh7th/cmp-cmdline",
+    "L3MON4D3/LuaSnip", -- Snippets plugin
+    "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+    "hrsh7th/nvim-cmp", -- Autocompletion plugin
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lua", -- For writing lua neovim specific
+    "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+    "hrsh7th/cmp-cmdline",
 
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
 
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
+    },
 
-	"windwp/nvim-autopairs",
+    "windwp/nvim-autopairs",
 })
