2c30af578ae33d30b0064f241da34342f02b89d6
Alexander Lopez <agl.alexglopez@gmail.com>
2025-05-02 17:09:45 -0700

    add zls settings

--- init.lua
+++ init.lua
@@ -617,8 +617,23 @@ vim.lsp.config["rust-analyzer"] = {
 
 vim.lsp.config["zls"] = {
     cmd = { "zls" },
-    root_markers = { "build.zig", "build.zig.zon" },
+    root_markers = { "build.zig", "build.zig.zon", "zls.json" },
     filetypes = { "zig" },
+    settings = {
+        zls = {
+            -- Whether to enable build-on-save diagnostics
+            --
+            -- Further information about build-on save:
+            -- https://zigtools.org/zls/guides/build-on-save/
+            enable_build_on_save = true,
+
+            -- Neovim already provides basic syntax highlighting
+            semantic_tokens = "partial",
+
+            -- omit if zig in path
+            -- zig_exe_path = '~/.local/bin/zig'
+        },
+    },
 }
 
 vim.lsp.config["marksman"] = {
