13f500c731c4f016857f87ba628f0a03fb82fb18
Alexander Lopez <agl.alexglopez@gmail.com>
2025-11-07 11:06:36 -0800

    update settings

--- init.lua
+++ init.lua
@@ -52,6 +52,19 @@ vim.cmd([[
     autocmd TermOpen * startinsert
     autocmd BufLeave term://* stopinsert
 ]])
+-- Easier visual cue for what is copied.
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+    pattern = "*",
+    desc = "highlight selection on yank",
+    callback = function()
+        vim.highlight.on_yank({ timeout = 200, visual = true })
+    end,
+})
+-- Resize splits
+vim.api.nvim_create_autocmd("VimResized", {
+    command = "wincmd =",
+})
 
 -- enable folding disabled for now.
 -- vim: foldmethod=marker foldlevel=0
